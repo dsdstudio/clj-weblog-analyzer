45ef34110818b7049d9780ede493411577eede9f
보롱킴 <dsdgun@gmail.com>
2015-08-08 23:11:56 +0900

    compojure / ring dependency 추가

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
+  :plugins [[lein-ring "0.9.6"]]
   :source-paths ["src"]
   :main weblog-analyzer.core
   :aot [weblog-analyzer.core])
