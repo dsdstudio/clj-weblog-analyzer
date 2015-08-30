ff1ece62342e470950de9ff68f5826159e1a4ca8
보롱킴 <dsdgun@gmail.com>
2015-08-30 23:43:47 +0900

    url 별 집계함수 구현

--- src/weblog_analyzer/core.clj
+++ src/weblog_analyzer/core.clj
@@ -38,7 +38,6 @@
       [in (java.util.zip.GZIPInputStream. (clojure.java.io/input-stream file))]
       (slurp in))))
 
-
 (defn tokenize-weblog 
   [log] 
   (-> (re-seq #"^([\d.]+)\ (\S+)\ (\S+)\ \[([\w:/]+\s[+\-]\d{4})\]\ \"(.+?)\"\ (\d{3})\ (\d+)\ \"([^\"]+)\"\ \"([^\"]+)\"" log)
@@ -76,6 +75,10 @@
 (defn group-by-day [coll]
   (sort-by first (for [m (group-by (fn [x] (datetime-to-str (:datetime x) "yyyyMMdd")) coll)]
     {(key m) (count (val m))})))
+
+(defn group-by-url [coll]
+  (for [m (group-by (fn [x] (:request x)) coll)]
+    {(key m) (count (val m))}))
 
 (defn -main [& args]
   (if (empty? args) (println "Usage: java -jar anl.jar [directorypath]")
