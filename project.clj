295c01182604c5584df0dde0bf6d208e45f59519
보롱킴 <dsdgun@gmail.com>
2015-09-02 02:13:43 +0900

    json 라이브러리 의존성 추가

--- project.clj
+++ project.clj
@@ -4,13 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [compojure "1.4.0"]
                  [ring/ring-core "1.4.0"]
                  [ring-server "0.3.1"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.3"]]
-  :plugins [[lein-ring "0.9.6"]]
+  :plugins [[lein-ring "0.9.6"]
+            [lein-sub "0.3.0"]]
   :source-paths ["src"]
   :main weblog-analyzer.core
   :aot [weblog-analyzer.core])
