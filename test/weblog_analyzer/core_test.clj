0f3c8ba0390c929632d97b53439d06e5a70db07e
보롱킴 <dsdgun@gmail.com>
2015-09-02 20:59:32 +0900

    쿠키 파싱 테스트 작성, 로그포맷변경으로 기존테스트가 의미없어져서 로그파싱 테스트 제거

--- test/weblog_analyzer/core_test.clj
+++ test/weblog_analyzer/core_test.clj
@@ -2,26 +2,19 @@
   (:require [clojure.test :refer :all]
             [weblog-analyzer.core :refer :all]))
 
-(def invalid-log "54.163.170.62 - - [05/Aug/2015:19:52:18 +0900] \"\" 400 0 \"-\" \"-\"")
-(def log-string 
-  "218.51.23.36 - - [25/Jul/2015:00:23:24 +0900] \"GET /api/pethostel/latelyjoin HTTP/1.1\" 200 1844 \"https://www.pethostel.net/\" \"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)\"")
-(def compare-log-map 
-  {:ip "218.51.23.36", 
-   :uid "-", 
-   :userid "-", 
-   :datetime (-> (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm:ss" java.util.Locale/ENGLISH)
-                 (.parse "2015-07-25 00:23:24")
-                 (.getTime))
-   :request "GET /api/pethostel/latelyjoin HTTP/1.1", 
-   :status "200", 
-   :bytes-sent "1844", 
-   :referer-url "https://www.pethostel.net/", 
-   :user-agent "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)"})
+(def sample-cookie "JSESSIONID=1BBC8036F143CE24A3497C4BE810811F; _ga=GA1.2.647868696.1441091332; _gat=1; _hjIncludedInSample=1; _hjUserId=fa0b20ba-0093-4cce-aa77-2e911638f763; X-PHAuthorization=v_46XkF2TeaXCsRbWBOQjg; PH-User=%7B%22sessionId%22%3A%22v_46XkF2TeaXCsRbWBOQjg%22%2C%22userId%22%3A%22X32BdlK7Rgq65Ah3ZDhw-Q%22%2C%22userEmail%22%3A%22ultrabar%40naver.com%22%2C%22userName%22%3A%22%ED%99%8D%EA%B0%80%ED%9D%AC%22%2C%22userImage%22%3A%7B%22imageKey%22%3A%22user%2F20150901%2F16%2FnVqT35SRSo6BFX350kLBNg%22%7D%2C%22hasHostel%22%3Afalse%2C%22isActiveHostel%22%3Afalse%2C%22finishCostSetup%22%3Afalse%2C%22remainDaysLastWorkingDay%22%3A0%7D")
+(def empty-cookie "-")
+(def null-cookie "null")
 
-(deftest serialize-log-test  
-  "로그라인 직렬화 처리를 검사한다"
-  (is (.equals compare-log-map (parse-log log-string))))
+(deftest empty-cookie-test
+  "빈 쿠키 테스트"
+  (is (= ["-"] (parse-cookie empty-cookie))))
 
-(deftest invalid-log-test 
-  "잘못된 로그를 검사한다."
-  (is (nil? (parse-log invalid-log))))
+(deftest null-cookie-test 
+  "빈 쿠키 테스트"
+  (is (= ["null"] (parse-cookie null-cookie))))
+
+(deftest cookie-test 
+  "테스트"
+  (println (parse-cookie sample-cookie))
+  (is (= 0 0)))
