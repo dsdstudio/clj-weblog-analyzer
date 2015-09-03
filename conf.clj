c6b37a66af66cff13352272dfd289df3d90a2672
보롱킴 <dsdgun@gmail.com>
2015-09-04 01:16:48 +0900

    ip filtering, session-key, campaignurl 설정 추가

--- conf.clj
+++ conf.clj
@@ -1,4 +1,6 @@
 {:inflow-channels {:naver "http://naver.com"
-                   :google "http://google.com"
-                   }
+                   :google "http://google.com"}
+ :session-key "JSESSIONID"
+ :campaign-urls #{"/?ref=email"}
+ :filtered-ip #{"218.51.23.36"}
 }
