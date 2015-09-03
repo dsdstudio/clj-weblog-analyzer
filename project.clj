6b112a4a816b0112ffdd13e588503bde8ea1f8b2
보롱킴 <dsdgun@gmail.com>
2015-09-03 22:43:46 +0900

    mongodb 의존성 추가, logging서버 구축을 위한 ring handler설정

--- project.clj
+++ project.clj
@@ -6,13 +6,17 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [compojure "1.4.0"]
+                 [com.stuartsierra/component "0.2.3"]
                  [ring/ring-core "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  [ring-server "0.3.1"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [com.novemberain/monger "3.0.0"]
                  [ch.qos.logback/logback-classic "1.1.3"]]
   :plugins [[lein-ring "0.9.6"]
             [lein-sub "0.3.0"]]
+  :ring {:handler weblog-analyzer.logger.handler/app}
   :source-paths ["src"]
-  :main weblog-analyzer.core
-  :aot [weblog-analyzer.core])
+  :main weblog-analyzer.analyzer.core
+  :aot [weblog-analyzer.analyzer.core])
