40a6e8fe9c7e9735f9c149d302757bbfa262e9c3
보롱킴 <dsdgun@gmail.com>
2015-09-11 21:53:29 +0900

    데이터 출력 함수 구현

--- src/weblog_analyzer/logger/handler.clj
+++ src/weblog_analyzer/logger/handler.clj
@@ -1,13 +1,17 @@
 (ns weblog-analyzer.logger.handler 
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [weblog-analyzer.analyzer.core :as anl]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defrecord Log [bzid, req, refefer, fv, sid, ua])
+(defrecord Log [bzid req refefer fv sid ua])
 (defroutes app-routes
-  (GET "/logging" request
-       ; TODO logging 처리
-       (str {:status 200 :msg ""}))
+  (GET "/logging" {params :params} (str params))
+  (GET "/data" [request] 
+       (apply 
+         str 
+         (anl/group-by-campaign-email-open
+           (anl/log-scan "email_logs"))))
   (route/not-found "Not Found"))
 
 (def app
