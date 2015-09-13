1ca6c3c672c8c2034ecfa1709054dd6f34364517
보롱킴 <dsdgun@gmail.com>
2015-09-13 22:56:15 +0900

    github url 변경

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject weblog-analyzer "0.1.0-SNAPSHOT"
   :description "simple clojure weblog analyzer"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/dsdstudio/clj-weblog-analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
